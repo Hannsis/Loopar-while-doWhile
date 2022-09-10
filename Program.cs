6ea1b081f964f9b0ba5f7fba65ecab370976b18a
Hanna Madsen <hannamymadsen89@gmail.com>
2022-09-10 21:09:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Loopar__while__doWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example();
+        }
+
+      
+        static void Example() 
+        {
+            Console.WriteLine("Skriv in ett ord");
+            Console.WriteLine("Skriv ordet 'Klar' för att avsluta");
+
+            string användarensOrd = Console.ReadLine();
+
+            while (användarensOrd != "Klar")
+            {
+                Console.WriteLine($"Du skrev in ordet {användarensOrd}");
+                Console.WriteLine("Skriv in ett ord till");
+                Console.WriteLine("Skriv ordet 'Klar' för att avsluta");
+                användarensOrd = Console.ReadLine();
+            }
+
+            Console.WriteLine("Du skrev ordet 'Klar', programmet kommer att avslutas");
+
+        }
+
+        static void Land() 
+        {
+            Console.WriteLine("Vilket är världens folkrikaste land?:");
+            string svar = Console.ReadLine();
+
+            while (svar != "Kina")
+            {
+                Console.WriteLine("Fel svar, försök igen");
+                svar = Console.ReadLine();
+            }
+            Console.WriteLine("Rätt svar");
+        }
+    }
+}

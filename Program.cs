02e8dbc2503c0f6c9fb644ce0d6ea85e51d9fc3c
Hanna Madsen <hannamymadsen89@gmail.com>
2022-09-11 10:29:50 +0200

    km till meter

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             //Parenteserna efter while ska innehålla ett booleskt uttryckt precis som en if-sats.
             //Allt som du skulle kunna skriva i en if-sats kan du också skriva i en while-loop.
             
-            FemtioBaklänges();
+            MenyProgramMiniräknare();
         }
 
 
@@ -65,9 +65,87 @@
         }
 
         static void MenyProgram() 
-        { 
-        
+        {
+            Console.WriteLine("Välj ett av följande alternativ.");
+            Console.WriteLine("1. Addera två tal");
+            Console.WriteLine("2. Multiplicera två tal");
+
+            string användarensVal = Console.ReadLine();
+
+            // Utför det val som användaren ville göra
+
+            switch (användarensVal) //EJ LOOP
+            {
+                case "1":
+                    Console.WriteLine("Skriv in två tal på var sin rad");
+                    double tal1 = double.Parse(Console.ReadLine());
+                    double tal2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{tal1}+{tal2}={tal1 + tal2}");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Skriv in två tal på var sin rad");
+                    double faktor1 = double.Parse(Console.ReadLine());
+                    double faktor2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{faktor1}*{faktor2}={faktor1 * faktor2}");
+                    break;
+
+                default:
+                    Console.WriteLine("Du valde inte ett giltigt alternativ.");
+                    break;
+            }
+
         }
-    
+
+        static void MenyProgramMiniräknare() 
+        {
+            Console.WriteLine("Detta är ett program som omvandlar KM till M och tvärtom. ");
+            string användarensVal = "";
+
+            while (användarensVal != "3")
+            {
+                Console.WriteLine("Välj vad du vill räkna på: ");
+                Console.WriteLine("1. KM till M");
+                Console.WriteLine("2. M till KM");
+                Console.WriteLine("3. Avsluta");
+
+                användarensVal = Console.ReadLine();
+                
+                switch (användarensVal)
+                {
+                    case "1":
+                        Console.WriteLine("tjolaho, KM till Meter.");
+                        Console.WriteLine("Skriv in km:");
+                        double kM = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"{kM} * 1000");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("tjolahej. meter till km.");                      
+                        Console.WriteLine("Skriv in km:");                        
+                        double m = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"{m} / 1000");
+                        break;
+                        break;
+
+                    case "3":
+                        Console.WriteLine("potatis");
+                        break;
+                  
+                    default:
+                        Console.WriteLine("Du valde inte ett giltigt alternativ.");
+                        break;
+
+                }
+            }
+
+        }
+          
     }
+
+    
 }
+
+    
+    
+
